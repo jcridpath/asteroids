a5c62bf7e66776e4f3a46bfe13bab74ea02dabf7
John Ridpath <john.ridpath@me.com>
2024-09-07 10:05:14 -0500

    Add .gitignore. Add main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.locals import *
+from constants import *
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_running = True
+
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
+    
