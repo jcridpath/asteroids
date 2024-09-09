47829d364a5acb092f6d9f83540a7b823cb8f53f
John Ridpath <john.ridpath@me.com>
2024-09-08 20:50:06 -0500

    Add rotation

--- main.py
+++ main.py
@@ -23,13 +23,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0, 0,0))
+        player.update(dt)
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
-
-
 
 if __name__ == "__main__":
     main()
