6201ed8d26fc922f5eca5cddf13bf9e01d70fd73
John Ridpath <john.ridpath@me.com>
2024-09-08 19:18:02 -0500

    Implement player ship

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    center_x = SCREEN_WIDTH / 2
+    center_y = SCREEN_HEIGHT / 2
+    player = Player(center_x, center_y)
+
+    dt = 0
+    clock = pygame.time.Clock()
     game_running = True
 
     while game_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
+
+        screen.fill((0, 0,0))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
+
+
 
 if __name__ == "__main__":
     main()
-    
+
