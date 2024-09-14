4caf805150bac6a1a40b9a834b752c304912e792
John Ridpath <john.ridpath@me.com>
2024-09-14 08:26:29 -0500

    Remove print staement used for debugging.

--- player.py
+++ player.py
@@ -7,6 +7,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
 
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -15,12 +16,13 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return[a, b, c]
 
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
     def rotate(self, dt):
         self.rotation = self.rotation + PLAYER_TURN_SPEED * dt
-        print(f"Rotation: {self.rotation}")
+
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
